7e3e8ba19aef9d6ca9792d7c8eca8d33abc5b4c3
urielduana <81336230+urielduana@users.noreply.github.com>
2023-01-26 15:58:44 -0600

    Tabla

--- resources/js/Pages/User.vue
+++ resources/js/Pages/User.vue
@@ -13,11 +13,31 @@ import AppLayout from '@/Layouts/AppLayout.vue'
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+
                     <pre>
-                        {{ users }}
+                        <table class="table-auto w-full text-center">
+                            <thead>
+                                <tr>
+                                    <th class="p-3">Id</th>
+                                    <th class="p-3">Nombre</th>
+                                    <th class="p-3">Email</th>
+                                    <th class="p-3">Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="u in users" :key="u.id">
+                                    <td class="p-3 border">{{u.id}}</td>
+                                    <td class="p-3 border">{{u.name}}</td>
+                                    <td class="p-3 border">{{u.email}}</td>
+                                    <td class="p-3 border">{{}}</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </pre>
                 </div>
             </div>
+        </div>
         </div>
     </AppLayout>
 </template>
